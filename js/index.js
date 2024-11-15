bc1cf12162b615edd3fb93bae53dce9c87fb8894
Santivivot <svivot@udesa.edu.ar>
2024-11-15 11:24:14 -0300

    avance js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+let recetas 
+let containerRecetas = document.querySelector('.container-recetas')
+
 fetch('https://dummyjson.com/recipes')
 .then(function(response){
     return response.json();
@@ -5,7 +8,21 @@ fetch('https://dummyjson.com/recipes')
 
 .then(function(data){
     console.log(data);
+    recetas = data.recipes
+    for(let i = 0;i<10;i++){
+        containerRecetas.innerHTML+=
+        `<article> 
+        <h3> ${recetas[i].name}</h3>
+        <img src= ${recetas[i].image}>
+        <p> ${recetas[i].difficulty} </p>
+        
+        </article>`
+        
+
+
+    }
 
 })
+
 
 
