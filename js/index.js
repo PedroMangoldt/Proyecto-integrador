12572be0135008a16b7d9f0ea5824f81a6ff4b24
PedroMangoldt <prodiguezmangoldt@udesa.edu.ar>
2024-11-15 12:56:05 -0300

    Segui el js con las recetas

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 let recetas 
 let containerRecetas = document.querySelector('.container-recetas')
 
-fetch('https://dummyjson.com/recipes')
+let limit = 10
+let skip = 0
+
+fetch(`https://dummyjson.com/recipes?limit=${limit}&skip=${skip}`)
 .then(function(response){
     return response.json();
 })
@@ -9,13 +12,41 @@ fetch('https://dummyjson.com/recipes')
 .then(function(data){
     console.log(data);
     recetas = data.recipes
-    for(let i = 0;i<10;i++){
+    for(let i = 0;i< recetas.length;i++){
         containerRecetas.innerHTML += `<article class="recetas"> 
         <h3> ${recetas[i].name}</h3>
         <img src= ${recetas[i].image}>
         <p> ${recetas[i].difficulty} </p>
         </article>`
     }
+    skip += limit;
+})
+.catch(function(error) {
+    console.log("Error cargando recetas:", error);
+});
+
+    let cargar = document.querySelector(".boton-cargar")
+
+    cargar.addEventListener("click", function(){
+
+        fetch(`https://dummyjson.com/recipes?limit=${limit}&skip=${skip}`)
+            .then(function(response){
+                return response.json();
+            })
+
+            .then(function(data){
+                console.log(data);
+                recetas = data.recipes
+                for(let i = 0;i<data.recipes.length;i++){
+                    containerRecetas.innerHTML += `<article class="recetas"> 
+                    <h3> ${recetas[i].name}</h3>
+                    <img src= ${recetas[i].image}>
+                    <p> ${recetas[i].difficulty} </p>
+                    </article>`
+                }
+            })
+            skip += limit;
+
 })
 
 
