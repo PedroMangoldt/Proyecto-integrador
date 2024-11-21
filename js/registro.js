2ab4c989af5d030164eb5f06f619c620789a2d5c
Santivivot <svivot@udesa.edu.ar>
2024-11-21 11:55:54 -0300

    Avance con el registro

---
+++ js/registro.js
@@ -0,0 +1,23 @@
+let formulario = document.querySelector(".formRegistro")
+let email = document.querySelector(`#email`)
+let contraseña = document.querySelector(`#contraseña`)
+let errorEmail = document.querySelector(`.email`)
+let errorContraseña = document.querySelector(`.contraseña`)
+
+formulario.addEventListener(`submit`, function(evento){
+    evento.preventDefault()
+    if(email.value == ``){
+        console.log(`Complete el campo`)
+        errorEmail.innerHTML = " <p> Completar lo requerido </p>"}
+    else if (contraseña.value == ``){
+        console.log(`Complete el campo`)
+        errorEmail.innerHTML = " <p> Completar lo requerido </p>"}
+    else {
+        formulario.submit()
+    }
+
+
+
+
+
+})
