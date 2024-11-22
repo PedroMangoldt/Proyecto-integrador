352ad6e1efd75afe9a4f82420a214ff74abf056a
PedroMangoldt <prodiguezmangoldt@udesa.edu.ar>
2024-11-22 11:14:32 -0300

    Agregue el campo requerido login

--- js/login.js
+++ js/login.js
@@ -1,9 +1,18 @@
 let formulario = document.querySelector("form")
-
 let campoEmail = document.querySelector("#email")
-
 let campoContraseña = document.querySelector("#contraseña")
+let errorEmail = document.querySelector(`.email`)
+let errorContraseña = document.querySelector(`.contraseña`)
 
 formulario.addEventListener(`submit`,function(evento){
     evento.preventDefault()
+    if(email.value == ``){
+        console.log(`Complete el campo`)
+        errorEmail.innerHTML = " <p> Completar lo requerido </p>"}
+    else if (contraseña.value == ``){
+        console.log(`Complete el campo`)
+        errorContraseña.innerHTML = " <p> Completar lo requerido </p>"}
+    else {
+        formulario.submit()
+    }
 })
